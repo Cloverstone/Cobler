695a6c6321b56b1902a3e7278d441e68a39202a4
Adam Smallcomb <atsmallcomb@gmail.com>
2016-02-12 14:15:40 -0500

    Revert content page

--- assets/js/content.js
+++ assets/js/content.js
@@ -8,22 +8,22 @@ list.addEventListener('click', function(e) {
   cb.collections[0].addItem($(e.target).closest('li').data('type'));
 })
 
-cbMain = new Cobler({ disabled: false,group:'fieldsets', targets: [$('.row')[0]],items:[[]]})
+// cbMain = new Cobler({ disabled: false,group:'fieldsets', targets: [$('.row')[0]],items:[[]]})
 
-document.getElementById('addFieldset').addEventListener('click', function(e) {
-  cbMain.collections[0].addItem('fieldset');
-  cbMain.on('change',function(s){
-    // debugger;
-    var temp = $('<div>').addClass('widget_container')
-    $('[name='+s.get().name+']').append(temp);
-    cb.addCollection(temp[0], [])
-  })
-  // cb.addCollection()
-})
-// mysortable = Sortable.create($('.row')[0])
-// function getOrdered() {
-//   var items = cb.toJSON(true);
-//   return _.map(mysortable.toArray(), function(item){
-//     return _.findWhere(items, {target: item})['items']
+// document.getElementById('addFieldset').addEventListener('click', function(e) {
+//   cbMain.collections[0].addItem('fieldset');
+//   cbMain.on('change',function(s){
+//     // debugger;
+//     var temp = $('<div>').addClass('widget_container')
+//     $('[name='+s.get().name+']').append(temp);
+//     cb.addCollection(temp[0], [])
 //   })
-// }
+//   // cb.addCollection()
+// })
+mysortable = Sortable.create($('.row')[0])
+function getOrdered() {
+  var items = cb.toJSON(true);
+  return _.map(mysortable.toArray(), function(item){
+    return _.findWhere(items, {target: item})['items']
+  })
+}
